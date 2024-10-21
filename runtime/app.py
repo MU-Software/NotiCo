b6f8191b46fedfd3cb4066a9eedd1d1563a3e091
MU-Software <musoftware@daum.net>
2024-10-21 21:59:08 +0900

    Fix SQS Handler to be able to be foundable

--- runtime/app.py
+++ runtime/app.py
@@ -1,3 +1,7 @@
+import json
+import logging
+import typing
+
 import chalice.app
 import chalicelib.config
 import chalicelib.route
@@ -5,5 +9,19 @@ import chalicelib.worker
 
 config = chalicelib.config.get_config()
 app = chalice.app.Chalice(app_name="notico")
+app.log.setLevel(logging.DEBUG)
+
 chalicelib.route.register_route(app)
-chalicelib.worker.register_worker(app, queue=config.infra.queue_name)
+
+
+@app.on_sqs_message(queue=config.infra.queue_name)
+def sqs_handler(event: chalice.app.SQSEvent) -> dict[str, typing.Any]:
+    try:
+        parsed_event = event.to_dict()
+        app.log.info(f"event: {parsed_event}")
+        result = chalicelib.worker.workers[json.loads(parsed_event["body"])["worker"]](event)
+        app.log.info(f"result: {result}")
+        return result
+    except Exception as e:
+        app.log.error(f"Failed to handle event: {parsed_event}", exc_info=e)
+        return {"error": "Failed to handle event"}
--- runtime/chalicelib/worker/__init__.py
+++ runtime/chalicelib/worker/__init__.py
@@ -1,15 +1,11 @@
-import json
-import logging
 import pathlib
 import typing
 
 import chalice.app
 import chalicelib.util.import_util as import_util
 
-logger = logging.getLogger(__name__)
-
 _WorkerCollectionType = dict[str, typing.Callable[[chalice.app.SQSEvent], dict[str, typing.Any]]]
-_workers: _WorkerCollectionType = {}
+workers: _WorkerCollectionType = {}
 
 for _path in pathlib.Path(__file__).parent.glob("*.py"):
     if _path.stem.startswith("__") or not (
@@ -24,19 +20,6 @@ for _path in pathlib.Path(__file__).parent.glob("*.py"):
     ):
         continue
 
-    if _duplicated := _workers.keys() & _patterns.keys():
+    if _duplicated := workers.keys() & _patterns.keys():
         raise ValueError(f"Worker {_duplicated} is already registered")
-    _workers.update(_patterns)
-
-
-def register_worker(app: chalice.app.Chalice, queue: str) -> None:
-    @app.on_sqs_message(queue=queue)
-    def handler(event: chalice.app.SQSEvent) -> dict[str, typing.Any]:
-        try:
-            parsed_event = event.to_dict()
-            result = _workers[json.loads(parsed_event["body"])["worker"]](event)
-            logger.info(f"Handled event: {parsed_event}")
-            return result
-        except Exception as e:
-            logger.error(f"Failed to handle event: {parsed_event}", exc_info=e)
-            return {"error": "Failed to handle event"}
+    workers.update(_patterns)
