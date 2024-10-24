be708f11f5a92507bc2dbf875901cfffa4e8b879
MU-Software <musoftware@daum.net>
2024-10-24 14:18:34 +0900

    Pass app and config to worker handler

--- runtime/chalicelib/worker/notification_sender.py
+++ runtime/chalicelib/worker/notification_sender.py
@@ -1,16 +1,22 @@
 import json
 import typing
 
+import chalice.app
+import chalicelib.config
 import chalicelib.sender as sender_module
 
 if typing.TYPE_CHECKING:
     import mypy_boto3_sqs.type_defs
 
 
-def notification_sender(record: "mypy_boto3_sqs.type_defs.MessageTypeDef") -> dict[str, typing.Any]:
+def notification_sender(
+    app: chalice.app.Chalice,
+    config: chalicelib.config.Config,
+    record: "mypy_boto3_sqs.type_defs.MessageTypeDef",
+) -> dict[str, typing.Any]:
     worker_payload = json.loads(record["body"])["worker_payload"]
     sender = sender_module.senders[worker_payload["sender_type"]]
-    return sender(worker_payload["sender_payload"])
+    return sender(worker_payload["sender_payload"] | {"conf": config})
 
 
 worker_patterns = {
--- runtime/chalicelib/worker/test_worker.py
+++ runtime/chalicelib/worker/test_worker.py
@@ -1,10 +1,17 @@
 import typing
 
+import chalice.app
+import chalicelib.config
+
 if typing.TYPE_CHECKING:
     import mypy_boto3_sqs.type_defs
 
 
-def test_handler(record: "mypy_boto3_sqs.type_defs.MessageTypeDef") -> "mypy_boto3_sqs.type_defs.MessageTypeDef":
+def test_handler(
+    app: chalice.app.Chalice,
+    config: chalicelib.config.Config,
+    record: "mypy_boto3_sqs.type_defs.MessageTypeDef",
+) -> "mypy_boto3_sqs.type_defs.MessageTypeDef":
     print(record)
     return record
 
