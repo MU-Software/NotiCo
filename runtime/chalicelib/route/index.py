0ec5f481564d19eb373d1dadbd1e667f9b5a1c06
MU-Software <musoftware@daum.net>
2024-12-05 19:49:38 +0900

    Change route path

--- runtime/chalicelib/route/index.py
+++ runtime/chalicelib/route/index.py
@@ -3,7 +3,7 @@ import chalice.app
 index_api = chalice.app.Blueprint(__name__)
 
 
-@index_api.route("/", methods=["GET"])
+@index_api.route("/service", methods=["GET"])
 def index() -> dict[str, str]:
     return {"service": "notico"}
 
