06a217a0463e3934b6cd96283e67e6bdfe86de4f
MU-Software <musoftware@daum.net>
2024-12-14 22:04:59 +0900

    Rewrite SQS notification sender worker

--- runtime/chalicelib/worker/notification_sender.py
+++ runtime/chalicelib/worker/notification_sender.py
@@ -1,8 +1,10 @@
-import json
+import functools
 import typing
 
 import chalice.app
-import chalicelib.sender as sender_module
+import chalicelib.send_manager as send_manager
+import chalicelib.send_manager.__interface__ as send_mgr_interface
+import pydantic
 
 if typing.TYPE_CHECKING:
     import mypy_boto3_sqs.type_defs
@@ -12,10 +14,36 @@ else:
     type RecordType = dict[str, typing.Any]
 
 
+class WorkerPayload(pydantic.BaseModel):
+    sender_type: str
+    sender_payload: dict[str, typing.Any]
+
+    @functools.cached_property
+    def send_manager(self) -> send_mgr_interface.SendManagerInterface:
+        return send_manager.send_managers[self.sender_type]
+
+    @functools.cached_property
+    def send_request_payload(self) -> send_mgr_interface.SendRequest:
+        return self.send_manager.send_request_cls.model_validate(self.sender_payload)
+
+    @pydantic.field_validator("sender_type", mode="before")
+    @classmethod
+    def validate_sender_type(cls, value: str) -> str:
+        if value not in send_manager.send_managers:
+            raise ValueError(f"Invalid sender type: {value}")
+        return value
+
+    @pydantic.model_validator(mode="after")
+    def validate_sender_payload(self) -> typing.Self:
+        self.send_request_payload
+        return self
+
+    def send(self) -> dict[str, str]:
+        return self.send_manager.send(self.send_request_payload)
+
+
 def notification_sender(app: chalice.app.Chalice, record: RecordType) -> dict[str, str]:
-    worker_payload = json.loads(record["body"])["worker_payload"]
-    sender = sender_module.senders[worker_payload["sender_type"]]
-    return sender(worker_payload["sender_payload"])
+    return WorkerPayload.model_validate_json(record["body"]).send()
 
 
 workers = [notification_sender]
